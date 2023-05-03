214d9432f99798e5c44d7b887069ce445c4f6ea8
mostafa_shawky <mostafashawky20177@gmail.com>
2023-05-03 12:15:50 +0300

    footer design

--- pages/_app.page.js
+++ pages/_app.page.js
@@ -1,35 +1,37 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import {
-  Hydrate,
-  QueryClient,
-  QueryClientProvider
-} from '@tanstack/react-query';
+import { Hydrate, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { LayoutWrapper } from '@root/components/layout';
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { LayoutWrapper } from "@root/components/layout";
 
-import { ThemeProvider } from 'react-bootstrap';
+import { ThemeProvider } from "react-bootstrap";
 
-import 'swiper/css';
-import 'react-toastify/dist/ReactToastify.css';
+import "swiper/css";
+import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.rtl.min.css";
-import '@root/sass/_app.scss';
+import "@root/sass/_app.scss";
 
 export default function App({ Component, pageProps }) {
+	const [queryClient] = useState(
+		() =>
+			new QueryClient({
+				defaultOptions: {
+					queries: { retry: false, refetchOnWindowFocus: false },
+				},
+			})
+	);
 
-  const [queryClient] = useState(() => new QueryClient({ defaultOptions: { queries: { retry: false } } }));
-
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <ThemeProvider dir="rtl">
-          <LayoutWrapper>
-            <Component {...pageProps} />
-          </LayoutWrapper>
-        </ThemeProvider>
-        <ReactQueryDevtools />
-      </Hydrate>
-    </QueryClientProvider>
-  )
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Hydrate state={pageProps.dehydratedState}>
+				<ThemeProvider dir="rtl">
+					<LayoutWrapper>
+						<Component {...pageProps} />
+					</LayoutWrapper>
+				</ThemeProvider>
+				<ReactQueryDevtools />
+			</Hydrate>
+		</QueryClientProvider>
+	);
 }
