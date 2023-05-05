1f4f5ba17c6dd3440ed2d63be1670e26f4989a3a
Mostafa shawy <mostafashawky20177@gmail.com>
2023-05-05 05:57:44 +0300

    merge

--- components/baricon/BarIcon.jsx
+++ components/baricon/BarIcon.jsx
@@ -1,17 +1,13 @@
-import style from './baricon.module.scss';
+// import style from './baricon.module.scss';
 
 const BarIcon = ({ activeIcons, barIconStyle = null, ...props }) => {
-
-    return (
-        <div
-            className={`${style.barIconWrapper} ${activeIcons ? style.activeButton : ''}`}
-            {...props}>
-
-            <span className={`${style.defaultBarIcon} ${barIconStyle ? barIconStyle : ''}`}></span>
-            <span className={`${style.defaultBarIcon} ${barIconStyle ? barIconStyle : ''}`}></span>
-            <span className={`${style.defaultBarIcon} ${barIconStyle ? barIconStyle : ''}`}></span>
-        </div>
-    )
-}
+	return (
+		<div className={`${style.barIconWrapper} ${activeIcons ? style.activeButton : ""}`} {...props}>
+			<span className={`${style.defaultBarIcon} ${barIconStyle ? barIconStyle : ""}`}></span>
+			<span className={`${style.defaultBarIcon} ${barIconStyle ? barIconStyle : ""}`}></span>
+			<span className={`${style.defaultBarIcon} ${barIconStyle ? barIconStyle : ""}`}></span>
+		</div>
+	);
+};
 
 export default BarIcon;
--- components/menus/menu/Menu.jsx
+++ components/menus/menu/Menu.jsx
@@ -1,72 +1,68 @@
-import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
 
-import { Container } from 'react-bootstrap';
-import { CategoriesMenu } from './categoriesmenu';
-import { BarIcon } from '@root/components/baricon';
+import { Container } from "react-bootstrap";
+import { CategoriesMenu } from "./categoriesmenu";
+import { BarIcon } from "@root/components/baricon";
 
-import style from './menu.module.scss';
+import style from "./menu.module.scss";
 
-const Menu = ({ sidebarMenuMobileIsOpen, setSidebarMenuMobileIsOpen, }) => {
+const Menu = ({ sidebarMenuMobileIsOpen, setSidebarMenuMobileIsOpen }) => {
+	const [fixedMenu, setFixedMenu] = useState(false);
 
-    const [fixedMenu, setFixedMenu] = useState(false);
+	useEffect(() => {
+		window.addEventListener("scroll", () => {
+			let scrollValue = document.documentElement.scrollTop;
+			scrollValue > 400 ? setFixedMenu(true) : setFixedMenu(false);
+		});
+	}, [setFixedMenu]);
 
-    useEffect(() => {
+	return (
+		<div className={`${style.menuWrapper} ${fixedMenu ? style.fixed : ""}`}>
+			<Container fluid="xl" className="d-flex align-items-center px-1">
+				<div className={style.categoryWrapper}>
+					<CategoriesMenu />
+				</div>
+				<ul className={`${style.listMenu} list-unstyled`}>
+					<li className={style.item}>
+						<Link href="/homepage">الصفحة الرئيسية</Link>
+					</li>
+					<li className={style.item}>
+						<Link href="/productsoffers">العروض المميزة</Link>
+					</li>
+					<li className={style.item}>
+						<Link href="/categories">الاقسام</Link>
+					</li>
+					<li className={style.item}>
+						<Link href="#">الطلبات</Link>
+					</li>
+					<li className={style.item}>
+						<Link href="/store">المتجر</Link>
+					</li>
+					<li className={style.item}>
+						<Link href="#">التواصل معنا</Link>
+					</li>
+				</ul>
 
-        window.addEventListener('scroll', () => {
-            let scrollValue = document.documentElement.scrollTop;
-            (scrollValue > 400) ? setFixedMenu(true) : setFixedMenu(false);
-        })
-
-    }, [setFixedMenu])
-
-    return (
-        <div className={`${style.menuWrapper} ${fixedMenu ? style.fixed : ''}`}>
-            <Container fluid="xl" className="d-flex align-items-center px-1">
-                <div className={style.categoryWrapper}>
-                    <CategoriesMenu />
-                </div>
-                <ul className={`${style.listMenu} list-unstyled`}>
-                    <li className={style.item}>
-                        <Link href="/homepage">الصفحة الرئيسية</Link>
-                    </li>
-                    <li className={style.item}>
-                        <Link href="/productsoffers">العروض المميزة</Link>
-                    </li>
-                    <li className={style.item}>
-                        <Link href="/categories">الاقسام</Link>
-                    </li>
-                    <li className={style.item}>
-                        <Link href="#">الطلبات</Link>
-                    </li>
-                    <li className={style.item}>
-                        <Link href="/store">المتجر</Link>
-                    </li>
-                    <li className={style.item}>
-                        <Link href="#">التواصل معنا</Link>
-                    </li>
-                </ul>
-
-                {/* display in mobile screen  */}
-                <div className={style.mobileWrapper}>
-                    <div className={`${style.logoMobileScreen}`}>
-                        <Link href="/homepage" className={style.logo}>
-                            Notify
-                            <span className={style.special}>
-                                Shop
-                            </span>
-                        </Link>
-                    </div>
-                    <div className={style.barWrapper}>
-                        <BarIcon
-                            activeIcons={sidebarMenuMobileIsOpen ? true : false}
-                            style={{ width: '20px', color: "#000" }}
-                            onClick={() => setSidebarMenuMobileIsOpen(!sidebarMenuMobileIsOpen)}
-                            barIconStyle={style.barIcon} />
-                    </div>
-                </div>
-            </Container>
-        </div>
-    )
-}
-export default Menu
+				{/* display in mobile screen  */}
+				<div className={style.mobileWrapper}>
+					<div className={`${style.logoMobileScreen}`}>
+						<Link href="/homepage" className={style.logo}>
+							Notify
+							<span className={style.special}>Shop</span>
+						</Link>
+					</div>
+					<div className={style.barWrapper}>
+						<BarIcon
+							activeIcons={sidebarMenuMobileIsOpen ? true : false}
+							style={{ width: "20px", color: "#000" }}
+							onClick={() => setSidebarMenuMobileIsOpen(!sidebarMenuMobileIsOpen)}
+							barIconStyle={style.barIcon}
+						/>
+					</div>
+				</div>
+			</Container>
+		</div>
+	);
+};
+export default Menu;
